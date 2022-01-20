864699c0c29eea9cf89b0ccf85d179c028e5dccb
ftriani <91723288+ftriani@users.noreply.github.com>
2022-01-20 14:46:37 +0700

    Add files via upload

---
+++ tugas js/latihan2.js
@@ -0,0 +1,16 @@
+const archon = ["tangsan", "xio u","asep", "lee"];
+
+for (let i =0; i < archon.length; i++){
+    console.log(archon[i]);
+}
+
+const gender = ['male','female'];
+let a =0;
+for (let a in gender) {
+    console.log(gender[a]) ;
+}
+
+const nikname = ["fitri","anggi","adah"];
+for(let x of nikname){
+    console.log(x);
+}
---
+++ tugas js/latihan3.js
@@ -0,0 +1,22 @@
+//Ganjil & Genap
+function bilangan(nomor) {
+     if (nomor % 2 == 0){
+         console.log('adalah genap')
+     } else {
+         console.log('adalah ganjil')
+     }
+}
+bilangan(357128)
+
+// rumus persegi 
+function persegi(sisi){
+    console.log(sisi * sisi)
+}
+
+persegi(9,9)
+
+//Rumus persegi panjang
+function persegipanjang(panjang,lebar){
+    console.log(panjang * lebar)
+}
+persegipanjang(25,13)
